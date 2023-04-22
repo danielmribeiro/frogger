4dfd453901f7ddd3a3a389892af4c5938461ac3c
Daniel Ribeiro <94848526+danielmribeiro@users.noreply.github.com>
2023-04-22 20:41:28 +0100

    Validation for Single Instance Running Added
    - Validates the running instance using Mutex
    - Close ServerApp correctly

--- Frogger/ServerApp/ServerApp.c
+++ Frogger/ServerApp/ServerApp.c
@@ -1,6 +1,9 @@
 #include "ServerApp.h"
 
 void log(TCHAR* logString);
+void check_single_instance(HANDLE hMutex);
+int close_serverapp(int errorCode);
+
 int _tmain(int argc, TCHAR* argv[]) {
 
 #ifdef UNICODE
@@ -9,15 +12,59 @@ int _tmain(int argc, TCHAR* argv[]) {
 	_setmode(_fileno(stderr), _O_WTEXT);
 #endif 
 
+	//WELCOME
 	log(MSG_SERVERAPP_WELCOME);
-	log(MSG_SERVERAPP_CLOSE_SUCCESS);
+	
+	//CHECK SINGLE INSTANCE RUNNING
+	HANDLE hMutex = NULL;
+	check_single_instance(hMutex);
 
-	exit(SUCCESS);
+	//CLOSE SERVER
+	close_serverapp(SUCCESS, hMutex);
 }
+
 /// <summary>
 /// Prints log strings
 /// </summary>
 /// <param name="logString"></param>
 void log(TCHAR* logString) {
 	_tprintf(_T("%s\n"), logString);
+}
+
+/// <summary>
+/// Checks if there's only one ServerApp instance running currently
+/// </summary>
+/// <param name="hMutex"></param>
+void check_single_instance(HANDLE hMutex) {
+	hMutex = CreateMutex(NULL, TRUE, FROGGER_SERVERAPP_ONLINE);
+	if (GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		CloseHandle(hMutex);
+		log(MSG_SERVERAPP_MULTIPLE_INSTANCES_RUNNING);
+		close_serverapp(ERROR_MULTIPLE_INSTANCES_RUNNING, hMutex);
+	}
+	else {
+		log(MSG_SERVERAPP_SINGLE_INSTANCE_RUNNING);
+	}
+}
+
+/// <summary>
+/// Closes the ServerApp correctly
+/// </summary>
+/// <param name="errorCode"></param>
+/// <param name="hMutex"></param>
+/// <returns></returns>
+int close_serverapp(int errorCode, HANDLE hMutex) {
+	switch (errorCode) {
+	case SUCCESS:
+		break;
+	case ERROR_MULTIPLE_INSTANCES_RUNNING:
+		break;
+	default:
+		break;
+	}
+	log(MSG_SERVERAPP_CLOSE_SUCCESS);
+	ReleaseMutex(hMutex);
+	CloseHandle(hMutex);
+	exit(errorCode);
 }
