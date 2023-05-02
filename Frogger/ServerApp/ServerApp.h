6ef00969ebfba6e96fa3f7728343586a9f1faf6b
Daniel Ribeiro <94848526+danielmribeiro@users.noreply.github.com>
2023-05-02 13:28:30 +0100

    Initial variables added

--- Frogger/ServerApp/ServerApp.h
+++ Frogger/ServerApp/ServerApp.h
@@ -8,6 +8,10 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+//LOGIC
+#define TRUE 1
+#define FALSE 0
+
 //TITLE MESSAGES
 #define MSG_SERVERAPP_TITLE_FROGGER _T("\n::::::: FROGGER - ServerApp :::::::")
 #define MSG_SERVERAPP_TITLE_CHECK_SINGLE_INSTANCE _T("\n------- CHECK SINGLE INSTANCE -------")
@@ -21,36 +25,24 @@
 #define MSG_SERVERAPP_TITLE_SAVE_NUMBER_OF_LANES _T("\n------- SAVE NUMBER OF LANES -------")
 #define MSG_SERVERAPP_TITLE_EXIT _T("\n------- EXIT FROGGER -------")
 
-
-
-
-
 //INFO MESSAGES
 #define MSG_SERVERAPP_INFO_SINGLE_INSTANCE_RUNNING _T("[INFO]: This ServerApp instance is the only one running currently!")
-
 #define MSG_SERVERAPP_INFO_SPEED_KEY_OPENED _T("[INFO]: Speed key was opened!")
 #define MSG_SERVERAPP_INFO_SPEED_KEY_CREATED _T("[INFO]: Speed key was created!")
 #define MSG_SERVERAPP_INFO_SPEED_KEY_READ _T("[INFO]: Speed key was read!")
 #define MSG_SERVERAPP_INFO_SPEED_KEY_CLOSED _T("[INFO]: Speed key was closed!")
 #define MSG_SERVERAPP_INFO_SPEED_KEY_SET _T("[INFO]: The value for the Speed was saved in the key!")
 #define MSG_SERVERAPP_INFO_SPEED_SET _T("[INFO]: The value for Speed was set!")
-
 #define MSG_SERVERAPP_INFO_NUMBER_OF_LANES_KEY_OPENED _T("[INFO]: Number Of Lanes key was opened!")
 #define MSG_SERVERAPP_INFO_NUMBER_OF_LANES_KEY_CREATED _T("[INFO]: Number Of Lanes key was created!")
 #define MSG_SERVERAPP_INFO_NUMBER_OF_LANES_KEY_READ _T("[INFO]: Number Of Lanes key was read!")
 #define MSG_SERVERAPP_INFO_NUMBER_OF_LANES_KEY_CLOSED _T("[INFO]: Number Of Lanes key was closed!")
 #define MSG_SERVERAPP_INFO_NUMBER_OF_LANES_KEY_SET _T("[INFO]: The value for the Number Of Lanes was saved in the key!")
 #define MSG_SERVERAPP_INFO_NUMBER_OF_LANES_SET _T("[INFO]: The value for Number Of Lanes was set!")
-
 #define MSG_SERVERAPP_INFO_ARGUMENT_FORMAT _T("[INFO]: You should use one of the following options:\n\t./ServerApp\n\t\tExample: ./ServerApp\n\n\t./ServerApp -s [SPEED]\n\t\tExample: ./ServerApp 50\n\n\t./ServerApp -l [NUMBER_OF_LANES]\n\t\tExample: ./ServerApp 5\n\n\t./ServerApp -s [SPEED] -l [NUMBER_OF_LANES]\n\t\tExample: ./server -s 50 -l 5\n\n\t./ServerApp -l [NUMBER_OF_LANES] -s [SPEED]\n\t\tExample: ./server -l 5 -s 50\n\n")
 #define MSG_SERVERAPP_INFO_SPEED_ARGUMENT_LIMIT _T("[INFO]: The speed value needs to be between 5 and 120")
 #define MSG_SERVERAPP_INFO_NUMBER_OF_LANES_ARGUMENT_LIMIT _T("[INFO]: The number of lanes value needs to be between 1 and 8")
-
 #define MSG_SERVERAPP_INFO_CLOSE_SUCCESS _T("[INFO]: ServerApp is closing successfully!")
-
-
-
-
 
 //ERROR MESSAGES
 #define MSG_SERVERAPP_ERROR_MULTIPLE_INSTANCES_RUNNING _T("[ERROR]: There's already a ServerApp instance running!")
@@ -63,10 +55,6 @@
 #define MSG_SERVERAPP_ERROR_CANT_OPEN_KEY _T("[ERROR]: Can't open the Key!")
 #define MSG_SERVERAPP_ERROR_CANT_CREATE_KEY _T("[ERROR]: Can't create the Key!")
 #define MSG_SERVERAPP_ERROR_CANT_SET_KEY _T("[ERROR]: Can't set the Key!")
-
-
-
-
 
 //ERRORS
 #define SUCCESS 0
@@ -81,44 +69,50 @@
 #define ERROR_CANT_CREATE_KEY 9
 #define ERROR_CANT_SET_KEY 10
 
-
-
-
-
 //MUTEX
 #define FROGGER_SERVERAPP_ONLINE _T("FROGGER_SERVERAPP_ONLINE")
-
-
-
-
-
-//SPEED
-#define SPEED_STR TEXT("-s")
-#define SPEED_MIN 5
-#define SPEED_MAX 120
-
-
-
-
-
-//NUMBER OF LANES
-#define NUMBER_OF_LANES_STR TEXT("-l")
-#define NUMBER_OF_LANES_MIN 1
-#define NUMBER_OF_LANES_MAX 8
-
-
-
-
 
 //REGISTRY
 #define KEYPATH_NAME _T("SOFTWARE\\Frogger\\")
 #define SPEED_KEY_NAME _T("SPEED")
 #define NUMBER_OF_LANES_KEY_NAME _T("NUMBEROFLANES")
 
+//SPEED
+#define SPEED_STR TEXT("-s")
+#define SPEED_MIN 5
+#define SPEED_MAX 120
+#define SPEED_STOPPED 0
 
+//NUMBER OF LANES
+#define NUMBER_OF_LANES_STR TEXT("-l")
+#define NUMBER_OF_LANES_MIN 1
+#define NUMBER_OF_LANES_MAX 8
+#define STARTLINE_DEFAULT 1
+#define FINISHLINE_DEFAULT 1
+#define LANES_MAX (STARTLINE_DEFAULT+NUMBER_OF_LANES_MAX+FINISHLINE_DEFAULT)
 
+//NUMBER OF COLUMNS IN THE LANES
+#define LANES_LENGTH 20
 
+//CARS
+#define CARS_MIN 0
+#define CARS_MAX (LANES_LENGTH+LANES_MAX)
+#define CARS_DEFAULT CARS_MIN
 
-//LOGIC
-#define TRUE 1
-#define FALSE 0
+//FROGS
+#define FROGS_MAX 2
+#define FROGS_INDIVIDUAL 1
+#define FROGS_COMPETITIVE 2
+
+//OBSTACLES
+#define OBSTACLES_MAX (LANES_LENGTH+LANES_MAX)
+
+//GAME PAWNS
+#define PAWN_FROG _T("F")
+#define PAWN_CAR _T("C")
+#define PAWN_TREE _T("T")
+
+//LIVES
+#define LIVES_MAX 3
+#define LIVES_DEFAULT LIVES_DEFAULT
+
